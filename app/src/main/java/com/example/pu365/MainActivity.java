3f1aecbb801218a34bef98311b8f3abb59d33a9a
Cornell Reddick <cornellreddick@outlook.com>
2022-03-25 10:47:25 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/pu365/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pu365;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       // Toolbar toolbar = findViewById(R.id.toolbar);
+    }
+}
