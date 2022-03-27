c43abf244c5161bf36ba641070288e681058b82e
Cornell Reddick <cornellreddick@outlook.com>
2022-03-27 18:36:57 -0400

    Added Menu xml

--- app/src/main/java/com/example/pu365/MainActivity.java
+++ app/src/main/java/com/example/pu365/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.pu365;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
